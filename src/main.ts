036e17070dd935a6c92d5209132745178b33cc7e
jintak0401 <jintak0401@naver.com>
2023-02-21 22:12:37 +0900

    feat: CORS 설정 추가

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,13 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
   const PORT = configService.get('PORT');
+  const CLIENT_URL = configService.get('CLIENT_URL');
+
+  app.enableCors({
+    origin: CLIENT_URL,
+    credentials: true,
+  });
+
   await app.listen(PORT || 3000);
 }
 
