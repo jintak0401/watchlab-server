3f76248ca4325830ef172c10d7251ce13ca38747
jintak0401 <jintak0401@naver.com>
2023-03-12 22:13:27 +0900

    feat: 포스트별 태그 리스트 얻는 api 추가

--- src/repository/tag.repo.ts
+++ src/repository/tag.repo.ts
@@ -7,6 +7,40 @@ import { Language } from 'generated/client';
 export class TagRepo {
   constructor(private readonly prisma: PrismaService) {}
 
+  async getAllTags() {
+    return this.prisma.tag.findMany({
+      select: {
+        name: true,
+      },
+    });
+  }
+
+  async getAllPostTags(language: Language) {
+    return this.prisma.tag.findMany({
+      where: {
+        posts: {
+          some: {
+            post: {
+              language,
+            },
+          },
+        },
+      },
+      select: {
+        name: true,
+        posts: {
+          select: {
+            post: {
+              select: {
+                slug: true,
+              },
+            },
+          },
+        },
+      },
+    });
+  }
+
   async getPostTags({ language, slug }: { language: Language; slug: string }) {
     return this.prisma.tag.findMany({
       where: {
