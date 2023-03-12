14deb3d48d700a6a3cf5fe439714e83f860e55dc
jintak0401 <jintak0401@naver.com>
2023-03-12 22:05:06 +0900

    feat: { [slug: string]: tag: string[] } 하게 만들어주는 util 함수 추가

--- src/utils/tag.utils.ts
+++ src/utils/tag.utils.ts
@@ -1,2 +1,20 @@
 export const flatTags = (tags: { name: string }[]) =>
   tags.map((tag) => tag.name);
+
+export const tags2obj = (
+  data: {
+    name: string;
+    posts: { post: { slug: string } }[];
+  }[],
+) => {
+  return data.reduce((acc, cur) => {
+    const { name, posts } = cur;
+    posts.forEach(({ post: { slug } }) => {
+      if (acc[slug] === undefined) {
+        acc[slug] = [];
+      }
+      acc[slug].push(name);
+    });
+    return acc;
+  }, {} as { [key: string]: string[] });
+};
